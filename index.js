0bf8712e1d803f8081b95e672b5257bc76d9ce3b
rokonroni <rokonuzzamanroni@gmail.com>
2023-10-19 18:32:59 +0600

    Single Product get API added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,6 +37,13 @@ async function setupServer() {
       const categoryName = req.params.brandName; 
       const query = { brand: categoryName };
       const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id; 
+      const query = { _id: new ObjectId(id)};
+      const result = await productsCollection.findOne(query);
       res.send(result);
     });
 
