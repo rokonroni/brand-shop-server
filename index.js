ae125cbc2f60ff409701b2feed362d8ebc446146
rokonroni <rokonuzzamanroni@gmail.com>
2023-10-20 19:23:06 +0600

    Cart Product Delete API added

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ async function setupServer() {
       const result = await cartProductCollection.find(query).toArray();
       res.send(result);
     });
+    
     app.delete("/cart/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
